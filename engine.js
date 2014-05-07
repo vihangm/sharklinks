2efc234f3d034e8f8a985c526d1ee5bd3ecbf4a7
Vihang Mehta <vihang@trifacta.com>
2014-05-07 00:52:29 -0700

    Remove unused function

--- engine.js
+++ engine.js
@@ -3,13 +3,6 @@ var logfmt = require("logfmt");
 var url = require('url');
 var app = express();
 
-function startsWith(str, starts){
-  if (starts === '') return true;
-  if (str === null || starts === null) return false;
-  str = String(str); starts = String(starts);
-  return str.length >= starts.length && str.slice(0, starts.length) === starts;
-}
-
 var exprs = [
   {
     pattern: /^diff$/,
