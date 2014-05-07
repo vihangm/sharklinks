cdf330e1afdf08286e5e18832bf5d21290716ac0
Vihang Mehta <vihang@trifacta.com>
2014-05-07 00:51:05 -0700

    Use RegExp and a JSON config

--- engine.js
+++ engine.js
@@ -5,46 +5,59 @@ var app = express();
 
 function startsWith(str, starts){
   if (starts === '') return true;
-  if (str == null || starts == null) return false;
+  if (str === null || starts === null) return false;
   str = String(str); starts = String(starts);
   return str.length >= starts.length && str.slice(0, starts.length) === starts;
 }
 
-function diff(res, param) {
-  return res.redirect('https://phab.trifacta.com/' + param.toUpperCase());
-}
-
-function alldiffs(res) {
-  return res.redirect('https://phab.trifacta.com/differential/');
-}
-
-function macro(res, param) {
-  if (param) {
-    return res.redirect('https://phab.trifacta.com/macro/?nameLike=' + param);
-  }
-  return res.redirect('https://phab.trifacta.com/macro/');
-}
-
-function issue(res, param) {
-  if (param) {
-    return res.redirect('https://trifacta.atlassian.net/browse/' + param);
-  }
-  return res.redirect('https://trifacta.atlassian.net/issues/?filter=-1');
-}
-
-function conf(res, param) {
-  if (param) {
-    return res.redirect('https://trifacta.atlassian.net/wiki/dosearchsite.action?queryString=' + param);
-  }
-  return res.redirect('https://trifacta.atlassian.net/wiki/');
-}
-
-function pr(res, param) {
-  if (param) {
-    return res.redirect('https://bitbucket.org/trifacta/trifacta/pull-request/' + param + '/');
-  }
-  return res.redirect('https://bitbucket.org/trifacta/trifacta/pull-requests');
-}
+var exprs = [
+  {
+    pattern: /^diff$/,
+    url: 'https://phab.trifacta.com/differential/',
+  },
+  {
+    pattern: /^[dD][0-9]+$/,
+    url: 'https://phab.trifacta.com/',
+    param: function(inp) { return inp.toUpperCase(); },
+  },
+  {
+    pattern: /^macro .+$/,
+    url: 'https://phab.trifacta.com/macro/?nameLike=',
+    param: function(inp) { return inp.slice(6, inp.length); },
+  },
+  {
+    pattern: /^macro$/,
+    url: 'https://phab.trifacta.com/macro/',
+  },
+  {
+    pattern: /^[pP][rR][0-9]*$/,
+    url: 'https://bitbucket.org/trifacta/trifacta/pull-request/',
+    param: function(inp) { return inp.slice(2, inp.length); },
+  },
+  {
+    pattern: /^[#][0-9]+$/,
+    url: 'https://bitbucket.org/trifacta/trifacta/pull-request/',
+    param: function(inp) { return inp.slice(1, inp.length); },
+  },
+  {
+    pattern: /^[tT][dD][-][0-9]+$/,
+    url: 'https://trifacta.atlassian.net/browse/',
+    param: function(inp) { return inp; },
+  },
+  {
+    pattern: /^t$/,
+    url: 'https://trifacta.atlassian.net/issues/?filter=-1',
+  },
+  {
+    pattern: /^w .+$/,
+    url: 'https://trifacta.atlassian.net/wiki/dosearchsite.action?queryString=',
+    param: function(inp) { return inp.slice(2, inp.length); },
+  },
+  {
+    pattern: /^w$/,
+    url: 'https://trifacta.atlassian.net/wiki/',
+  },
+];
 
 app.use(logfmt.requestLogger());
 
@@ -59,31 +72,15 @@ app.get('/search', function(req, res) {
   if (parsed.protocol) {
     return res.redirect(parsed.href);
   }
-  if (query === 'diff') {
-    return alldiffs(res);
+
+  for (var key in exprs) {
+    var expr = exprs[key];
+    if (expr.pattern.test(query)) {
+      return expr.param ? res.redirect(expr.url + expr.param(query)) : res.redirect(expr.url);
+    }
   }
-  if (query === 't') {
-    return issue(res);
-  }
-  if (startsWith(query, 'd') || startsWith(query, 'D')) {
-    return diff(res, query);
-  }
-  if (startsWith(query, 'TD-')) {
-    return issue(res, query);
-  }
-  if (startsWith(query, 'PR')) {
-    return pr(res, query.slice(2, query.length));
-  }
-  if (startsWith(query, 'w ')) {
-    return conf(res, query.slice(2, query.length));
-  }
-  if (startsWith(query, 'macro ')) {
-    return macro(res, query.slice(6, query.length));
-  }
-  if (query === 'macro') {
-    return macro(res);
-  }
-  res.redirect('https://www.google.com/search?q=' + req.query.q)
+
+  res.redirect('https://www.google.com/search?q=' + query);
 });
 
 var port = Number(process.env.PORT || 5000);
